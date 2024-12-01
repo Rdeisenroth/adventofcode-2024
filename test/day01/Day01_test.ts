ae4e02ffa9f7105eff018a85ada6a6a09f49db95
Ruben Deisenroth <rdeisenroth@gmail.com>
2024-12-01 06:21:56 +0100

    day 1

--- test/day01/Day01_test.ts
+++ test/day01/Day01_test.ts
@@ -6,18 +6,18 @@ const dayImpl = new Day01();
 
 Deno.test("test part 1", () => {
     const input = dayImpl.getInput(true, 1);
-    // const result = dayImpl.solvePart1(input);
-    // assertEquals(result, "1");
-    assertThrows(() => {
-        dayImpl.solvePart1(input);
-    }, "Method not implemented.");
+    const result = dayImpl.solvePart1(input);
+    assertEquals(result, "11");
+    // assertThrows(() => {
+    //     dayImpl.solvePart1(input);
+    // }, "Method not implemented.");
 });
 
 Deno.test("test part 2", () => {
     const input = dayImpl.getInput(true, 2);
-    // const result = dayImpl.solvePart2(input);
-    // assertEquals(result, "2");
-    assertThrows(() => {
-        dayImpl.solvePart2(input);
-    }, "Method not implemented.");
+    const result = dayImpl.solvePart2(input);
+    assertEquals(result, "31");
+    // assertThrows(() => {
+    //     dayImpl.solvePart2(input);
+    // }, "Method not implemented.");
 });
