c4a3edd347f588b973e2935c10cb887379874059
Ruben Deisenroth <rdeisenroth@gmail.com>
2024-12-01 06:21:56 +0100

    day 1

--- src/day01/Day01.ts
+++ src/day01/Day01.ts
@@ -1,3 +1,4 @@
+import { lines } from "@utils/util.ts";
 // @deno-types="@types/lodash"
 import _ from "lodash";
 // deno-lint-ignore no-unused-vars
@@ -14,15 +15,39 @@ export class Day01 extends AdventOfCodeDay {
     }
     // deno-lint-ignore no-unused-vars
     solvePart1(input: string): string {
-        throw new Error("Method not implemented.");
+        const left: number[] = [];
+        const right: number[] = [];
+        lines(input).map((line) => line.split(/\s+/) as string[]).forEach(l => {
+            left.push(parseInt(l[0])); right.push(parseInt(l[1]))
+        });
+        // sort the left and right arrays in ascending order
+        left.sort((a, b) => a - b);
+        right.sort((a, b) => a - b);
+        let sum = 0;
+        for (let i = 0; i < left.length; i++) {
+            const distance = Math.abs(left[i] - right[i]);
+            sum += distance;
+        }
+        return sum.toString();
     }
     // deno-lint-ignore no-unused-vars
     solvePart2(input: string): string {
-        throw new Error("Method not implemented.");
+        const left: number[] = [];
+        const right: number[] = [];
+        lines(input).map((line) => line.split(/\s+/) as string[]).forEach(l => {
+            left.push(parseInt(l[0])); right.push(parseInt(l[1]))
+        });
+        let sum = 0;
+        for (let i = 0; i < left.length; i++) {
+            const l = left[i];
+            const rAppearences = right.filter(r => r === l).length;
+            sum += l * rAppearences;
+        }
+        return sum.toString();
     }
 }
 
 if (import.meta.main) {
     const day = new Day01();
-    day.run();
+    day.runTest();
 }
